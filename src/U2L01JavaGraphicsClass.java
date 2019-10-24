89ba242fa1dd45cd3fee7c74d14e8cc47be3fb5d
Eleanor BW <ebarthwu@gmail.com>
2019-10-24 10:11:06 -0400

    completed Noodle Games logo!

--- src/U2L01JavaGraphicsClass.java
+++ src/U2L01JavaGraphicsClass.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 
 import javax.swing.JFrame;
@@ -14,14 +16,49 @@ public class U2L01JavaGraphicsClass extends JPanel {
 	    
 	    frame.add(panel);
 
-	    frame.setSize(300, 200);
+	    frame.setSize(500, 500);
 	    frame.setVisible(true);
 
 	}
 	
 	public void paintComponent(Graphics g){
-		g.drawLine(0,0,50,50);       
-		g.drawLine(50,0,50,75);
+		Color tomatoesSuck = new Color (252, 48, 85);
+		Color meanGreenBeans = new Color (94, 235, 73);
+		
+		//g.drawLine(0,0,50,50); 
+		g.setColor(tomatoesSuck);
+		//g.drawOval(75,75,75,75);
+		g.fillOval(120, 120, 220, 220);
+		
+		g.setColor(meanGreenBeans);
+		int x [] = {230, 180, 210};
+		int y [] = {110, 140, 160};
+		g.fillPolygon(x, y, 3);
+		
+		int xOne [] = {230, 220, 240};
+		int yOne [] = {110, 160, 150};
+		g.fillPolygon(xOne, yOne, 3);
+		
+		int xTwo [] = {230, 280, 260};
+		int yTwo [] = {110, 140, 160};
+		g.fillPolygon(xTwo, yTwo, 3);
+		
+		int xThird [] = {230, 190, 190};
+		int yThird [] = {110, 100, 120};
+		g.fillPolygon(xThird, yThird, 3);
+		
+		int xFourth [] = {230, 270, 270};
+		int yFourth [] = {110, 90, 110};
+		g.fillPolygon(xFourth, yFourth, 3);
+		
+		//g.drawRect (225, 105, 20, 60);
+		//g.fillRect (225, 70, 10, 60);
+		
+		g.setColor(Color.WHITE);
+		g.setFont(new Font("Arial", Font.BOLD, 32));
+		g.drawString("Games", 180, 270);
+		g.drawString("Noodle", 180, 220);
+		
 	}
 
 }
